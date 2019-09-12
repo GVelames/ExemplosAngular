d3e6472be1083f36413ae26c101aedbd85930df3
Gilvan <gvelames@gmail.com>
2019-09-11 22:12:58 -0300

    Atualizar com novos exemplos do HttpClient

--- example-httpget/src/app/home/home.component.ts
+++ example-httpget/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ConsultarNomesService } from "src/app/services/consultar-nomes.service";
+import { SafeUrl } from "@angular/platform-browser";
 
 @Component({
     selector: 'home',
@@ -8,15 +9,42 @@ import { ConsultarNomesService } from "src/app/services/consultar-nomes.service"
 export class HomeComponent {
 
     nomes: string[];
+    imagem: SafeUrl;
 
     constructor(private consultarNomes$: ConsultarNomesService){}
 
-    listarNomes(){
-        this.consultarNomes$.listarNomes().subscribe(
+    ngOnInit() {
+        this.nomes = [];
+    }
+
+    listarNomesFromServer(){
+        this.consultarNomes$.listarNomesFromServer().subscribe(
             response => this.nomes = response,
             error => console.log(error)
         );
     }
+
+    listarNomesFromJson(){
+        this.consultarNomes$.listarNomesFromJson().subscribe(
+            response => this.nomes = response,
+            error => console.log(error)
+        );
+    }
+
+    listarNomesFromTxt(){
+        this.consultarNomes$.listarNomesFromTxt().subscribe(
+            response => this.nomes = response,
+            error => console.log(error)
+        );
+    }
+
+    carregarImagem(){
+        this.consultarNomes$.carregarImagem().subscribe(
+            response => this.imagem = response,
+            error => console.log(error)
+        );
+    }
+
 
     limparNomes() {
         this.nomes = [];
--- example-httpget/src/app/services/consultar-nomes.service.ts
+++ example-httpget/src/app/services/consultar-nomes.service.ts
@@ -2,18 +2,33 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, throwError, of } from "rxjs";
 import { map, tap, catchError }   from 'rxjs/operators';
+import { DomSanitizer } from "@angular/platform-browser";
 
 
 @Injectable()
 export class ConsultarNomesService {
 
-    private url: string = "http://www.mocky.io/v2/5d75a2c13100004b3395064b"
 
-    constructor(private http: HttpClient) {}
+    /** Url com os nomes */
+    private url: string = "http://www.mocky.io/v2/5d75a2c13100004b3395064b";
+    /** Caminho do arquivo json */
+    private arquivoJson: string = "assets/nomes.json";
+    /** Caminho do arquivo txt */
+    private arquivoTxt: string = "assets/nomes.txt";
+    /** Caminho da imagem */
+    private caminhoImagem: string = "/favicon.ico";
 
-    listarNomes(): Observable<any> {
+
+
+    constructor(private http$: HttpClient, private domSanitizer: DomSanitizer) {}
+
+
+    /**
+     * Listar os nomes do servidor
+     */
+    listarNomesFromServer(): Observable<any> {
         // Para usar operadores do RXJS, eles devem estar dentro do método PIPE.
-        return this.http.get(this.url).pipe(
+        return this.http$.get(this.url).pipe(
             // Usamos este operador para manipular o item recebido e enviar o resultado para o ouvinte
             map((response) => {
                 response[0] = 'Gilvan Sobrenome';
@@ -34,6 +49,39 @@ export class ConsultarNomesService {
                 console.log("Ocorreu um erro na execucao", err);
                 return throwError("Error thrown from catchError");
                 // return of(["Erro na execucao"]);
+            })
+        );
+    }
+
+
+    /**
+     * Listar nomes de um arquivo json
+     */
+    listarNomesFromJson(): Observable<any> {
+        return this.http$.get(this.arquivoJson);
+    }
+
+
+    /**
+     * Listar nomes de um arquivo texto
+     */
+    listarNomesFromTxt(): Observable<any> {
+        // Por default o responseType é json. Como estamos buscando um arquivo texto, devemos passar o valor 'text'
+        return this.http$.get(this.arquivoTxt, { responseType: 'text' }).pipe(
+            map(response => response.split(","))
+        );
+    }
+
+
+    /**
+     * Carregar uma imagem de determinado diretorio
+     */
+    carregarImagem(): Observable<any> {
+        // Para buscar arquivos, devemos utilizar o responseType 'blob'. Uma ponto de atenção. Caso tenha que retorna um Observable<Blob>
+        // devemos passar o seguinte valor no responseType: 'blob' as 'json'
+        return this.http$.get(this.caminhoImagem, { responseType: 'blob' }).pipe(
+            map( response => {
+                return this.domSanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(response));
             })
         );
     }
